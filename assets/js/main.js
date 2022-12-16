a87f0fc1dbe101c5555f5682c79c68a665f2db3a
Vinicius Santos <vinicius.santosmatheus1@gmail.com>
2022-12-16 19:01:18 -0300

    ADD

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,8 +69,19 @@ tabs.forEach(tab =>{
 })
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+modalBtns = document.querySelectorAll('.services__button'),
+modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
 
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
 /*==================== PORTFOLIO SWIPER  ====================*/
 
 
